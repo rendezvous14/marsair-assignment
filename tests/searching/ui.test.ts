3e4a6a7d83d46fa1f94cc0e8463082bfb1898b46
Nattapol Wilarat <nattapol.wilarat@bytedance.com>
2023-11-06 22:01:32 +0800

    add test.beforeEach for enter landing page

--- tests/searching/ui.test.ts
+++ tests/searching/ui.test.ts
@@ -1,45 +1,48 @@
 import { test, expect } from '@playwright/test'
 const base_url = process.env.base_url
 
-test('has title', async ({ page }) => {
-  // open page
-  await page.goto(`${base_url}`)
+test.describe('Mars Airlines: Links back to home', async () => {
+  test.beforeEach(async ({ page }) => {
+    // open page
+    await page.goto(`${base_url}`)
 
-  await expect(
-    page.getByRole('heading', { name: 'Welcome to MarsAir!' })
-  ).toBeVisible()
+    // Ensure the landing page is ready
+    await expect(
+      page.getByRole('heading', { name: 'Welcome to MarsAir!' })
+    ).toBeVisible()
 
-  await expect(
-    page.getByRole('heading', { name: 'Book a ticket to the red planet now!' })
-  ).toBeVisible()
+    await expect(
+      page.getByRole('heading', {
+        name: 'Book a ticket to the red planet now!',
+      })
+    ).toBeVisible()
+  })
 
-  await page.getByLabel('Departing').selectOption('0')
-  await page.getByLabel('Returning').selectOption('0')
-  await page.getByLabel('Promotional Code').focus()
-  await page.getByLabel('Promotional Code').fill('ABC')
-  await page.getByRole('button', { name: 'Search' }).click()
-  await page.getByRole('heading', { name: 'Search Results' }).click()
-  await expect(
-    page.getByText('Sorry, there are no more seats available.')
-  ).toBeVisible()
+  test('Click "Back" button on the search result window', async ({ page }) => {
+    // Enter the valid Departing and Returning date
+    await page.getByLabel('Departing').selectOption('0')
+    await page.getByLabel('Returning').selectOption('0')
+    await page.getByLabel('Promotional Code').focus()
+    await page.getByLabel('Promotional Code').fill('ABC')
+    await page.getByRole('button', { name: 'Search' }).click()
 
-  await page.getByRole('link', { name: 'Back' }).click()
+    await expect(
+      page.getByRole('heading', { name: 'Search Results' })
+    ).toBeVisible()
+    await expect(
+      page.getByText('Sorry, there are no more seats available.')
+    ).toBeVisible()
 
-  await expect(
-    page.getByRole('heading', { name: 'Welcome to MarsAir!' })
-  ).toBeVisible()
+    await page.getByRole('link', { name: 'Back' }).click()
 
-  await expect(
-    page.getByRole('heading', { name: 'Book an ticket to the red planet now!' })
-  ).toBeVisible()
+    await expect(
+      page.getByRole('heading', { name: 'Welcome to MarsAir!' })
+    ).toBeVisible()
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Book a ticket to the red planet now!',
+      })
+    ).toBeVisible()
+  })
 })
-
-// test('open the website', async ({ page }) => {
-// await page.goto(`${base_url}`)
-// // Click the get started link.
-// await page.getByRole('link', { name: 'Get started' }).click()
-// // Expects page to have a heading with the name of Installation.
-// await expect(
-//   page.getByRole('heading', { name: 'Installation' })
-// ).toBeVisible()
-// })
