2a5a1246678ef39b047502d24941113532b0c41d
Nattapol Wilarat <nattapol.wilarat@bytedance.com>
2023-11-06 22:20:21 +0800

    add back navigation on browser

--- tests/searching/ui.test.ts
+++ tests/searching/ui.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 const base_url = process.env.base_url
 
-test.describe('Mars Airlines: Links back to home', async () => {
+test.describe.parallel('Mars Airlines: Links back to home', async () => {
   test.beforeEach(async ({ page }) => {
     // open page
     await page.goto(`${base_url}`)
@@ -34,6 +34,79 @@ test.describe('Mars Airlines: Links back to home', async () => {
     ).toBeVisible()
 
     await page.getByRole('link', { name: 'Back' }).click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Welcome to MarsAir!' })
+    ).toBeVisible()
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Book a ticket to the red planet now!',
+      })
+    ).toBeVisible()
+  })
+
+  test('Click the "Mars Air" Logo on search result page', async ({ page }) => {
+    // Enter the valid Departing and Returning date
+    await page.getByLabel('Departing').selectOption('0')
+    await page.getByLabel('Returning').selectOption('0')
+    await page.getByLabel('Promotional Code').focus()
+    await page.getByLabel('Promotional Code').fill('ABC')
+    await page.getByRole('button', { name: 'Search' }).click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Search Results' })
+    ).toBeVisible()
+    await expect(
+      page.getByText('Sorry, there are no more seats available.')
+    ).toBeVisible()
+
+    // click on logo
+    await page.getByRole('link', { name: 'MarsAir' }).click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Welcome to MarsAir!' })
+    ).toBeVisible()
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Book a ticket to the red planet now!',
+      })
+    ).toBeVisible()
+  })
+
+  test('Click the back navigation on the browswer', async ({ page }) => {
+    // Enter the valid Departing and Returning date
+    await page.getByLabel('Departing').selectOption('0')
+    await page.getByLabel('Returning').selectOption('0')
+    await page.getByLabel('Promotional Code').focus()
+    await page.getByLabel('Promotional Code').fill('ABC')
+    await page.getByRole('button', { name: 'Search' }).click()
+
+    await expect(
+      page.getByRole('heading', { name: 'Search Results' })
+    ).toBeVisible()
+    await expect(
+      page.getByText('Sorry, there are no more seats available.')
+    ).toBeVisible()
+
+    // click on logo
+    await page.goBack()
+
+    await expect(
+      page.getByRole('heading', { name: 'Welcome to MarsAir!' })
+    ).toBeVisible()
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Book a ticket to the red planet now!',
+      })
+    ).toBeVisible()
+  })
+
+  test('Click the "Mars Air" Logo on home page', async ({ page }) => {
+    // click on logo
+    await page.getByRole('link', { name: 'MarsAir' }).click()
 
     await expect(
       page.getByRole('heading', { name: 'Welcome to MarsAir!' })
